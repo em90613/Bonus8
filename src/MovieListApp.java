f9f2e76bfc87def9bc0ce852c5fdd005fa59f758
Eric Mackey <eric.mackey06@gmail.com>
2017-07-10 08:59:14 -0400

    Added to list. Program runs, but not sure how. Need validation.

--- src/MovieListApp.java
+++ src/MovieListApp.java
@@ -7,6 +7,20 @@ public class MovieListApp {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         String userInput = "";
+        ArrayList<Movie> movieList1 = getMovies();
+        ArrayList<Movie> category;
+
+        System.out.println("Welcome to The Movie App List");
+        System.out.println();
+        System.out.println("What genre of movie would you like to watch: ");
+        userInput = scnr.nextLine();
+        category = getSelectedMovies(userInput, movieList1);
+        for (Movie title : category) {
+            System.out.println(title);
+        }
+    }
+
+    public static ArrayList<Movie> getMovies() {
         ArrayList<Movie> movieList1 = new ArrayList<>();
         ArrayList<Movie> category = new ArrayList<>();
 
@@ -15,10 +29,40 @@ public class MovieListApp {
         moana.setCategory("animated");//sets the genre to animated
         movieList1.add(moana);
 
+        Movie howToTrain = new Movie();
+        howToTrain.setMovieTitle("How To Train Your Dragon");
+        howToTrain.setCategory("animated");
+        movieList1.add(howToTrain);
+
+        Movie cars = new Movie();
+        cars.setMovieTitle("Cars");
+        cars.setCategory("animated");
+        movieList1.add(cars);
+
+        Movie horribleBosses = new Movie();
+        horribleBosses.setMovieTitle("Horrible Bosses");
+        horribleBosses.setCategory("comedy");
+        movieList1.add(horribleBosses);
+
+        Movie bridesMaids = new Movie();
+        bridesMaids.setMovieTitle("Brides Maids");
+        bridesMaids.setCategory("comedy");
+        movieList1.add(bridesMaids);
+
         Movie talladegaNights = new Movie();
         talladegaNights.setMovieTitle("Talladega Nights");
         talladegaNights.setCategory("comedy");
         movieList1.add(talladegaNights);
+
+        Movie manchester = new Movie();
+        manchester.setMovieTitle("Manchester By the Sea");
+        manchester.setCategory("drama");
+        movieList1.add(manchester);
+
+        Movie goodWill = new Movie();
+        goodWill.setMovieTitle("Good Will Hunting");
+        goodWill.setCategory("drama");
+        movieList1.add(goodWill);
 
         Movie theDeparted = new Movie();
         theDeparted.setMovieTitle("The Departed");
@@ -30,14 +74,18 @@ public class MovieListApp {
         halloween.setCategory("horror");
         movieList1.add(halloween);
 
-        System.out.println("Welcome to The Movie App List");
-        System.out.println();
-        System.out.println("What genre of movie would you like to watch: ");
-        userInput = scnr.nextLine();
-        category = getSelectedMovies(userInput, movieList1);
-        for (Movie title : category) {
-            System.out.println(title);
-        }
+
+        Movie conjuring = new Movie();
+        conjuring.setMovieTitle("The Conjuring");
+        conjuring.setCategory("horror");
+        movieList1.add(conjuring);
+
+        Movie thanksKilling = new Movie();
+        thanksKilling.setMovieTitle("Thankskilling");
+        thanksKilling.setCategory("horror");
+        movieList1.add(thanksKilling);
+
+        return movieList1;
     }
 
     public static ArrayList<Movie> getSelectedMovies ( String userInput, ArrayList<Movie> movies){
